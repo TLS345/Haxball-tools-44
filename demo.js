a83c64b836cab4cda37c7e6cd1f26c4425c0799b
TLS <124143520+TLS345@users.noreply.github.com>
2025-11-09 00:25:19 -0300

    Create  demo.js

---
+++ demo.js
@@ -0,0 +1,17 @@
+console.log("This is a demo for the Haxball Discord Bot.");
+console.log("Simulating command !haxrooms abc123...\n");
+
+const room = {
+  id: "abc123",
+  name: "Bazinga 1v1",
+  players: 5,
+  maxPlayers: 12,
+  password: false,
+  flag: "🇦🇷",
+  lat: -34.6037,
+  long: -58.3816,
+  version: 12,
+  occupancyBar: "🟧🟧🟧🟧🟧🟧⬛⬛⬛⬛ 60%"
+};
+
+console.log(room);
